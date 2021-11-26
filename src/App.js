6caf003a68c3bf783f0ef84ee5d94a42571d004a
Dacomis <albei.andrei@gmail.com>
2021-11-26 10:35:37 +0200

    Add pulse animation and congigure tailwind animation duration and opacity

--- src/App.js
+++ src/App.js
@@ -14,18 +14,18 @@ export default function App() {
       </div>
 
       {/* right */}
-      <div className="absolute bg-purple-600 opacity-20 rounded-full w-80 h-80 -top-14 left-36" />
-      <div className="absolute bg-pink-600 opacity-10 rounded-full w-48 h-48 left-9" />
-      <div className="absolute bg-pink-200 opacity-20 rounded-full w-56 h-56 -bottom-2 -left-9" />
-      <div className="absolute bg-indigo-200 opacity-20 rounded-full w-40 h-40 -bottom-16 left-28" />
+      <div className="absolute animate-pulse bg-purple-600 opacity-20 rounded-full w-80 h-80 -top-14 left-36" />
+      <div className="absolute animate-pulse bg-pink-600 opacity-10 rounded-full w-48 h-48 left-9" />
+      <div className="absolute animate-pulse bg-pink-200 opacity-20 rounded-full w-56 h-56 -bottom-2 -left-9" />
+      <div className="absolute animate-pulse bg-indigo-200 opacity-20 rounded-full w-40 h-40 -bottom-16 left-28" />
 
       {/* left */}
-      <div className="absolute bg-indigo-200 opacity-20 rounded-full w-28 h-28 -top-10 right-1/3" />
-      <div className="absolute bg-pink-200 opacity-20 rounded-full w-32 h-32 top-4 right-36" />
-      <div className="absolute bg-purple-600 opacity-20 rounded-full w-52 h-52 top-36 right-14" />
-      <div className="absolute bg-pink-600 opacity-10 rounded-full w-44 h-44 top-24 right-48" />
-      <div className="absolute bg-pink-200 opacity-20 rounded-full w-24 h-24 bottom-64 -right-12" />
-      <div className="absolute bg-purple-600 opacity-50 rounded-full w-80 h-80 -bottom-8 -right-36" />
+      <div className="absolute animate-pulse bg-indigo-200 opacity-20 rounded-full w-28 h-28 -top-10 right-1/3" />
+      <div className="absolute animate-pulse bg-pink-200 opacity-20 rounded-full w-32 h-32 top-4 right-36" />
+      <div className="absolute animate-pulse bg-purple-600 opacity-20 rounded-full w-52 h-52 top-36 right-14" />
+      <div className="absolute animate-pulse bg-pink-500 opacity-10 rounded-full w-44 h-44 top-24 right-48" />
+      <div className="absolute animate-pulse bg-pink-200 opacity-20 rounded-full w-24 h-24 bottom-64 -right-12" />
+      <div className="absolute animate-pulse bg-purple-600 opacity-50 rounded-full w-80 h-80 -bottom-8 -right-36" />
 
       <SearchHash />
       {/* <div class="has-tooltip">
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
       backgroundImage: (theme) => ({
         "hero-pattern": "url('/src/assets/waves.png')",
       }),
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: "0.25",
+          },
+          "50%": {
+            opacity: ".1",
+          },
+        },
+      },
+      animation: {
+        pulse: "pulse 10s cubic-bezier(0.4, 0, 0.6, 1) infinite;",
+      },
     },
   },
   variants: {
