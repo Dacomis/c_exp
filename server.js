38ccd1a6b519bb49c0de8edceac3d84f3bbc3ec4
Dacomis <albei.andrei@gmail.com>
2022-01-21 20:00:21 +0200

    Made modifications to server and package json

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("src/db.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults({ static: "./build" });
 const port = process.env.PORT || 3000;
+// const cors = require("cors");
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+
+// server.use(cors(corsOptions));
 
 server.use(middlewares);
 server.use(router);
