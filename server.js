5661e70d966a26c10309bd2607a4da350458fb55
Dacomis <albei.andrei@gmail.com>
2022-01-25 14:34:23 +0200

    Add env and rename build folder

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
+// const cors = require("cors");
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-const cors = require("cors");
-
-server.use(cors());
+// server.use(cors(corsOptions));
 
 server.use(middlewares);
 server.use(router);
